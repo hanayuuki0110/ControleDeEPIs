b1fa998ad79154e97dc2e529fd984176ba28cc79
hanayuuki0110 <hanayuuki0110@gmail.com>
2025-11-25 04:10:39 +0900

    crie controller

--- src/main/java/com/example/demo/controller/EpiController.java
+++ src/main/java/com/example/demo/controller/EpiController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.epi.EpiRequest;
+import com.example.demo.dto.epi.EpiResponse;
 import com.example.demo.service.EpiService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Epis")
+
 public class EpiController {
 
 private final EpiService servicoEpi;
@@ -14,4 +21,18 @@ public EpiController(EpiService servicoEpi) {
     this.servicoEpi = servicoEpi;
 
 }
+@PostMapping
+
+    public ResponseEntity<Void> cadastrarEpi(@Valid @RequestBody EpiRequest epiRequest){
+
+    servicoEpi.cadastrarEpi(epiRequest);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+
+}
+@GetMapping
+
+    public ResponseEntity<List<EpiResponse>> listarEpi(){
+    return ResponseEntity.status(HttpStatus.OK).body(servicoEpi.listarEpis());
+}
+
 }
--- src/main/java/com/example/demo/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/controller/FuncionarioController.java
@@ -1,4 +1,37 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.dto.funcionario.FuncionarioRequest;
+import com.example.demo.dto.funcionario.FuncionarioResponse;
+import com.example.demo.service.FuncionarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class FuncionarioController {
+
+    private final FuncionarioService servicoFuncionario;
+
+    public FuncionarioController(FuncionarioService servicoFuncionario) {
+        this.servicoFuncionario = servicoFuncionario;
+
+    }
+    @PostMapping
+
+    public ResponseEntity<Void> cadastrarEpi(@Valid @RequestBody FuncionarioRequest funcionarioRequest){
+
+        servicoFuncionario.cadastrarFuncionario(funcionarioRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+    @GetMapping
+
+    public ResponseEntity<List<FuncionarioResponse>> listarEpi(){
+        return ResponseEntity.status(HttpStatus.OK).body(servicoFuncionario.listarFuncionarios());
+    }
 }
