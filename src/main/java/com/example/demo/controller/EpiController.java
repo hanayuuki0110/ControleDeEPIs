7d945366bfd0a3f2066e794c9fbe8b9cfb2665ee
hanayuuki0110 <hanayuuki0110@gmail.com>
2025-11-25 02:52:03 +0900

    adicione dependencia spring-boot-starter-web

--- src/main/java/com/example/demo/controller/EpiController.java
+++ src/main/java/com/example/demo/controller/EpiController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.EpiService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Epis")
 public class EpiController {
+
+private final EpiService servicoEpi;
+
+public EpiController(EpiService servicoEpi) {
+    this.servicoEpi = servicoEpi;
+
+}
 }
